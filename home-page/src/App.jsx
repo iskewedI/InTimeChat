31efc85d2407e32677adc60762dad1f6ba27e1ef
iskewed <jtornello2019@hotmail.com>
2021-01-26 18:48:29 -0300

    Added project

---
+++ home-page/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Container } from 'shards-react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'shards-ui/dist/css/shards.min.css';
+
+import './index.css';
+
+import Chat from 'chat/Chat';
+
+const App = () => (
+  <div>
+    <Container>
+      <p>Live embedded:</p>
+      <h1>
+        <Chat />
+      </h1>
+    </Container>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
